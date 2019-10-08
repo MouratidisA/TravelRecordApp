8719b65cc866c18efa8953a2a5502123fced1c22
Mouratidis Anargyros <argiris95@windowslive.com>
2019-10-08 16:05:34 +0300

    Granting and Displaying user's location

--- TravelRecordApp/TravelRecordApp.Android/MainActivity.cs
+++ TravelRecordApp/TravelRecordApp.Android/MainActivity.cs
@@ -7,6 +7,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.Permissions;
 using Environment = System.Environment;
 
 namespace TravelRecordApp.Droid
@@ -23,12 +24,20 @@ namespace TravelRecordApp.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             global::Xamarin.FormsMaps.Init(this, savedInstanceState);
 
+            Plugin.CurrentActivity.CrossCurrentActivity.Current.Init(this ,savedInstanceState);
+
             string dbName = "travel_db.sqlite";
             string folderPath = System.Environment.GetFolderPath(Environment.SpecialFolder.Personal);
             string fullPath = System.IO.Path.Combine(folderPath, dbName);
 
 
             LoadApplication(new App(fullPath));
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode,permissions,grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
--- TravelRecordApp/TravelRecordApp/MapPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/MapPage.xaml.cs
@@ -1,20 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace TravelRecordApp
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MapPage : ContentPage
-	{
-		public MapPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            InitializeComponent();
+            GetPermissions();
+        }
+
+        private async void GetPermissions()
+        {
+            try
+            {
+
+
+                var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.LocationWhenInUse);
+                if (status != PermissionStatus.Granted)
+                {
+                    if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission
+                        .LocationWhenInUse))
+                        await DisplayAlert("Need your location", "We need to access your location", "Ok");
+
+                    var results = await CrossPermissions.Current.RequestPermissionsAsync(Permission.LocationWhenInUse);
+                    if (results.ContainsKey(Permission.LocationWhenInUse))
+                        status = results[Permission.LocationWhenInUse];
+                }
+
+                if (status == PermissionStatus.Granted)
+                {
+                    locationsMap.IsShowingUser = true;
+                }
+                else
+                {
+                    await DisplayAlert("Need your location", "We need to access your location", "Ok");
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "Ok");
+            }
+        }
+    }
 }
