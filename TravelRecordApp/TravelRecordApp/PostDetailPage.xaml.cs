e99c3abc15e882650f6fb2535bb45db203636b4b
Mouratidis Anargyros <argiris95@windowslive.com>
2019-10-10 15:22:01 +0300

    Update Post Detail Page with new Elements in View and Azure Db
    Remove SQLite functionality

--- TravelRecordApp/TravelRecordApp/PostDetailPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/PostDetailPage.xaml.cs
@@ -7,44 +7,79 @@ using Xamarin.Forms.Xaml;
 namespace TravelRecordApp
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class PostDetailPage : ContentPage
+    public partial class PostDetailPage : ContentPage
     {
         private Post selectedPost;
 
-        public PostDetailPage (Post selectedPost)
-		{
-			InitializeComponent ();
+        public PostDetailPage(Post selectedPost)
+        {
+            InitializeComponent();
             this.selectedPost = selectedPost;
             ExperienceEntry.Text = selectedPost.Experience;
+            VenueLabel.Text = selectedPost.VenueName;
+            CategoryLabel.Text = selectedPost.CategoryName;
+            AddressLabel.Text = selectedPost.Address;
+            CoordinatesLabel.Text = $"{selectedPost.Latitude}, {selectedPost.Longitude}";
+            DistanceLabel.Text = $"{selectedPost.Distance} m";
+
 
         }
 
-        private void UpdateButton_Clicked(object sender, EventArgs e)
+        private async void UpdateButton_Clicked(object sender, EventArgs e)
         {
             selectedPost.Experience = ExperienceEntry.Text;
-            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
-            {
-                conn.CreateTable<Post>();
-                int rows = conn.Update(selectedPost);
 
-                if (rows > 0)
-                    DisplayAlert("Success", "Experience successfully updated", "Ok");
-                else
-                    DisplayAlert("Failure", "Experience failed to be updated", "Ok");
+            #region SQLite Implementation
+            //using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            //{
+            //    conn.CreateTable<Post>();
+            //    int rows = conn.Update(selectedPost);
+
+            //    if (rows > 0)
+            //        DisplayAlert("Success", "Experience successfully updated", "Ok");
+            //    else
+            //        DisplayAlert("Failure", "Experience failed to be updated", "Ok");
+            //}
+            #endregion
+
+            try
+            {
+                await App.MobileService.GetTable<Post>().UpdateAsync(selectedPost);
+                await DisplayAlert("Success", "Experience successfully updated", "Ok");
             }
+            catch (Exception)
+            {
+                await DisplayAlert("Failure", "Experience failed to be updated", "Ok");
+            }
+
+
         }
-        private void DeleteButton_Clicked(object sender, EventArgs e)
+        private async void DeleteButton_Clicked(object sender, EventArgs e)
         {
-            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
-            {
-                conn.CreateTable<Post>();
-                int rows = conn.Delete(selectedPost);
+            #region SQLite Implementation
+            //using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            //{
+            //    conn.CreateTable<Post>();
+            //    int rows = conn.Delete(selectedPost);
 
-                if (rows > 0)
-                    DisplayAlert("Success", "Experience successfully deleted", "Ok");
-                else
-                    DisplayAlert("Failure", "Experience failed to be deleted", "Ok");
+            //    if (rows > 0)
+            //        DisplayAlert("Success", "Experience successfully deleted", "Ok");
+            //    else
+            //        DisplayAlert("Failure", "Experience failed to be deleted", "Ok");
+            //}
+            #endregion
+
+            try
+            {
+                await App.MobileService.GetTable<Post>().DeleteAsync(selectedPost);
+                await DisplayAlert("Success", "Experience successfully deleted", "Ok");
             }
+            catch (Exception)
+            {
+
+                await DisplayAlert("Failure", "Experience failed to be deleted", "Ok");
+            }
+
         }
     }
 }
