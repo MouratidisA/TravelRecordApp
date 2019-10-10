c86b6a5661607679b5b61d4b30fabd84f6e8d257
Mouratidis Anargyros <argiris95@windowslive.com>
2019-10-10 14:40:11 +0300

    User Login From azure database added

--- TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using TravelRecordApp.Model;
 using Xamarin.Forms;
 
 namespace TravelRecordApp
@@ -13,20 +15,29 @@ namespace TravelRecordApp
             IconImage.Source = ImageSource.FromResource("TravelRecordApp.Assets.Images.plane.png", assembly);
         }
 
-        private void LoginButton_Clicked(object sender, EventArgs e)
+        private async void LoginButton_Clicked(object sender, EventArgs e)
         {
-           bool isEmailEmpty= string.IsNullOrEmpty(EmailEntry.Text);
-           bool isPasswordEmpty = string.IsNullOrEmpty(PasswordEntry.Text);
+            bool isEmailEmpty = string.IsNullOrEmpty(EmailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(PasswordEntry.Text);
 
 
-           if (isEmailEmpty||isPasswordEmpty)
-           {
+            if (isEmailEmpty || isPasswordEmpty)
+            {
 
-           }
-           else
-           {
-               Navigation.PushAsync(new HomePage());
-           }
+            }
+            else
+            {
+                var user = (await App.MobileService.GetTable<User>().Where(u => u.Email == EmailEntry.Text).ToListAsync()).FirstOrDefault();
+                if (user != null)
+                {
+                    if (user.Password == PasswordEntry.Text)
+                        await Navigation.PushAsync(new HomePage());
+                    else
+                        await DisplayAlert("Error", "Email or Password are incorrect", "Ok");
+                }
+                else
+                    await DisplayAlert("Error", "There was an error logging you in", "Ok");
+            }
         }
 
         private void RegisterButton_Clicked(object sender, EventArgs e)
