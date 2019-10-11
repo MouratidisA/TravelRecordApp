34568bf6731bf27ffdafdcceedb61ff8d8baaa9c
Mouratidis Anargyros <argiris95@windowslive.com>
2019-10-11 13:10:32 +0300

    Requesting Permission for location in New Trave Page

--- TravelRecordApp/TravelRecordApp/NewTravelPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/NewTravelPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Plugin.Geolocator;
+﻿using System.Linq;
+using Plugin.Geolocator;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
 using TravelRecordApp.Model;
 using TravelRecordApp.ViewModel;
 using Xamarin.Forms;
@@ -23,13 +26,41 @@ namespace TravelRecordApp
         {
             base.OnAppearing();
 
-            var locator = CrossGeolocator.Current;
-            var position = await locator.GetPositionAsync();
-            var venues = await Venue.GetVenues(position.Latitude, position.Longitude);
+            try
+            {
+                var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Plugin.Permissions.Abstractions.Permission.Location);
+                if (status != PermissionStatus.Granted)
+                {
+                    if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Plugin.Permissions
+                        .Abstractions.Permission.Location))
+                    {
+                        await DisplayAlert("Need Permission", "We will have to access your location", "Ok");
+                    }
+
+                    var result = await CrossPermissions.Current.RequestPermissionsAsync(Permission.Location);
+
+                    if (result.ContainsKey(Permission.Location))
+                        status = result[Permission.Location];
+                }
 
 
-            VenueListView.ItemsSource = venues;
+                if (status == PermissionStatus.Granted)
+                {
+                    var locator = CrossGeolocator.Current;
+                    var position = await locator.GetPositionAsync();
+                    var venues = await Venue.GetVenues(position.Latitude, position.Longitude);
+                    VenueListView.ItemsSource = venues;
+                }
+                else
+                {
+                    await DisplayAlert("No Permission", "You didn't granted permission to access your location, we cannot proceed", "Ok");
+                }
 
+            }
+            catch (System.Exception)
+            {
+
+            }
         }
     }
 }
