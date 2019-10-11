fe5b7cfb459031f022356e617e2ef30fd5ea1342
Mouratidis Anargyros <argiris95@windowslive.com>
2019-10-11 09:43:04 +0300

    Implementing ICommand & Binding Commands

--- TravelRecordApp/TravelRecordApp/HomePage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/HomePage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using TravelRecordApp.ViewModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,14 +7,14 @@ namespace TravelRecordApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : TabbedPage
     {
+        private HomeViewModel viewModel;
+
         public HomePage()
         {
             InitializeComponent();
+            viewModel = new HomeViewModel();
+            BindingContext = viewModel;
         }
 
-        private void AddNewTravel_Clicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new NewTravelPage());
-        }
     }
 }
---
+++ TravelRecordApp/TravelRecordApp/ViewModel/Commands/NavigationCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TravelRecordApp.ViewModel.Commands
+{
+    public class NavigationCommand :ICommand
+    {
+        public HomeViewModel HomeVm { set; get; }
+        public event EventHandler CanExecuteChanged;
+
+        public NavigationCommand(HomeViewModel homeVm)
+        {
+            HomeVm = homeVm;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            HomeVm.Navigate();
+        }
+
+        
+    }
+}
